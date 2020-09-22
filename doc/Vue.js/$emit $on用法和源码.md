27f80577f3c5fb44f9cd067f556767b362b9d1b7
lijh11 <lijh11@yusys.com.cn>
2020-09-22 23:04:51 +0800

    add $emit $on

---
+++ doc/Vue.js/$emit $on用法和源码.md
@@ -0,0 +1,21 @@
+$emit $on使用：
+
+```javascript
+this.dataList.push(22);
+```
+
+
+### $emit的源码实现
+
+
+
+```javascript
+// 原生数组原型
+
+```
+
+### $on的源码实现
+
+```javascript
+// todo
+```
--- doc/design-patterns/test.js
+++ doc/design-patterns/test.js
@@ -1,32 +1,6 @@
-const arrayProto = Array.prototype;
-const arrayMethod = Object.create(arrayProto);
-
-["push", "pop", "shift", "unshift", "splice", "sort", "reverse"].forEach(
-  (method) => {
-    const originalMethod = arrayMethod[method];
-    Object.defineProperty(arrayMethod, method, {
-      configurable: true,
-      writable: true,
-      enumerable: false,
-      value: function (...arg) {
-        return originalMethod.apply(this, arg);
-      },
-    });
+class Person{
+  constructor() {
+    this.x = 10;
   }
-);
-
-let arr = [1, 2, 3];
-let p = new Proxy(arr, {
-  get: function (target, key) {
-    switch (key) {
-      case "push":
-    }
-  },
-  set: function (target, key, value) {
-    console.log("set: ", arguments);
-  },
-});
-console.log(p);
-p[1];
-p[1] = "a";
-console.log(arr, p);
+}
+console.log(Person.x);
